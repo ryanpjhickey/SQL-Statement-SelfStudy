ae91050b17e7b95616e4da1241179ddb961b1de6
ryanpjhickey <ryanpjhickey@gmail.com>
2023-03-15 00:47:47 -0700

    upload SelectFromClause

---
+++ SelectFromClause.sql
@@ -0,0 +1,13 @@
+use fcc_sql_guides_database
+
+select studentID, FullName, sat_score, recordUpdated
+from student
+where
+  (
+    studentID between 1 and 5
+    or studentID = 8
+    or FullName like '%Maximo%'
+  )
+and sat_score NOT in (1000, 1400);
+
+/*get student info for students between id 1-5, id 8 itself, or FullName contains Maximo, only if SAT score is not between 1000-1400
